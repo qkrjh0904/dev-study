d1f97df492fc604e6288f570a4766c52ad4ee02d
jeongho <qkrjh0904@gmail.com>
2023-07-06 16:15:24 +0900

    할인 정책 변경

--- 오브젝트/2장. 객체지향 프로그래밍/java/Movie.java
+++ 오브젝트/2장. 객체지향 프로그래밍/java/Movie.java
@@ -26,4 +26,8 @@ public class Movie {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
 }
